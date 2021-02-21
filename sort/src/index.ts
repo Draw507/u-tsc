0afe80d48cc233a1720e4bde5a6747b48f5ef6c6
David Robinson <drobinson@assanet.com>
2021-02-21 10:59:01 -0500

    sort use abstract class for reusable implementation

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -2,6 +2,5 @@ import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 
 const numbersCollection = new NumbersCollection([5, -9, 0, 8, 7, 6]);
-const sort = new Sorter(numbersCollection);
-sort.sort();
-console.log(sort.collection);
+numbersCollection.sort();
+console.log(numbersCollection.data);
