5b11ae03f01960aaedd53b3dc77a687c54ef2f6f
David Robinson <drobinson@assanet.com>
2021-02-21 15:29:42 -0500

    decorators catch errors

---
+++ basics/decorators.ts
@@ -0,0 +1,28 @@
+class Boat {
+  color: string = "red";
+
+  get formattedColor(): string {
+    return `This boats color is ${this.color}`;
+  }
+
+  @logError
+  pilot(): void {
+    throw new Error();
+    console.log("swish");
+  }
+}
+
+function logError(target: any, key: string, desc: PropertyDescriptor): void {
+  //   console.log("Target: ", target);
+  //   console.log("Key: ", key);
+  const method = desc.value;
+  desc.value = function () {
+    try {
+      method();
+    } catch (e) {
+      console.log("Oops, boat was sunk");
+    }
+  };
+}
+
+new Boat().pilot();
