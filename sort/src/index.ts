12ca18d230f336c548ccd368eccdb630aa6da5bb
David Robinson <drobinson@assanet.com>
2021-02-21 10:05:45 -0500

    sort

---
+++ sort/src/index.ts
@@ -0,0 +1,23 @@
+class Sorter {
+  constructor(public collection: number[]) {
+    this.collection = collection;
+  }
+
+  sort(): void {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const temp = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = temp;
+        }
+      }
+    }
+  }
+}
+
+const sort = new Sorter([5, -9, 0, 8, 7, 6]);
+sort.sort();
+console.log(sort.collection);
