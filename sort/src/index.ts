3225bd5cb8b9fb04e364d2368f13c8de758b0db6
David Robinson <drobinson@assanet.com>
2021-02-21 10:46:36 -0500

    sort with interface

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,23 +1,7 @@
-class Sorter {
-  constructor(public collection: number[]) {
-    this.collection = collection;
-  }
+import { Sorter } from "./Sorter";
+import { NumbersCollection } from "./NumbersCollection";
 
-  sort(): void {
-    const { length } = this.collection;
-
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const temp = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = temp;
-        }
-      }
-    }
-  }
-}
-
-const sort = new Sorter([5, -9, 0, 8, 7, 6]);
+const numbersCollection = new NumbersCollection([5, -9, 0, 8, 7, 6]);
+const sort = new Sorter(numbersCollection);
 sort.sort();
 console.log(sort.collection);
