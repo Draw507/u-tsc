bbde90e55b99a6a9fc61de2e30c0d8dbe70be658
David Robinson <drobinson@assanet.com>
2021-02-21 08:39:33 -0500

    maps

---
+++ maps/src/Company.ts
@@ -0,0 +1,20 @@
+import faker from "faker";
+import { IMappable } from "./CustomMap";
+
+export class Company implements IMappable {
+  name: string;
+  catchPhrase: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.name = faker.company.companyName();
+    this.catchPhrase = faker.company.catchPhrase();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+}
